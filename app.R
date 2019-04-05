f57d8f1cce2c091144eadd1e5d698b83f4914a9a
Henrik Bengtsson <hb@aroma-project.org>
2019-04-04 20:33:52 -0700

    Adding site selector

--- app.R
+++ app.R
@@ -8,24 +8,13 @@ stopifnot(timezone() == "America/Los_Angeles")
 locations <- list(
   ## "https://forecast.weather.gov/MapClick.php?lat=37.461100&lon=-121.864600&FcstType=digitalDWML"
   "Ed Levin, CA (300 ft)" = list(
-    name = "Ed Levin, CA (300 ft)",
+    name = "Ed Levin, CA (300 ft & 600 ft)",
     seealso = c(
       BHGC = "http://bhgc.org/sites/",
       WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
     ),
     lat=  37.4611,
     lon=-121.8646
-  ),
-
-  ## "https://forecast.weather.gov/MapClick.php?lat=37.461324&lon=-121.859979&FcstType=digitalDWML"
-  "Ed Levin, CA (600 ft)" = list(
-    name = "Ed Levin, CA (600 ft)",
-    seealso = c(
-      BHGC = "http://bhgc.org/sites/",
-      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
-    ),
-    lat=  37.4613,
-    lon=-121.8600
   ),
 
   ## "https://forecast.weather.gov/MapClick.php?lat=37.475389&lon=-121.861305&FcstType=digitalDWML"
@@ -40,13 +29,13 @@ locations <- list(
   )
 )
 
-location <- locations[["Ed Levin, CA (600 ft)"]]
-location <- locations[["Ed Levin, CA (1750 ft)"]]
-
+selected_location <- "Ed Levin, CA (1750 ft)"
 if (!exists("db")) db <- list()
-if (is.null(db$values)) {
+
+if (is.null(db[[selected_location]])) {
+  location <- locations[[selected_location]]
   url <- noaa_url(lat=location$lat, lon=location$lon)
-  db$values <- read_noaa(url)
+  db[[selected_location]] <- read_noaa(url)
 }
 
 options(flavor = "narrow")
@@ -78,8 +67,9 @@ ui <- fluidPage(
                                 });
                             ')),
     
+      selectInput("site", "Choose a flying site:", names(locations), selected = selected_location),
       strong(sprintf("Site: %s", location$name)), br(),
-      "Source: ", a("NOAA", href = noaa_url(lat=location$lat, lon=location$lon, format = "html")), sprintf("(%s)", as.character(attr(db$values, "last_updated"), usetz = TRUE)), br(),
+      "Source: ", a("NOAA", href = noaa_url(lat=location$lat, lon=location$lon, format = "html")), sprintf("(%s)", as.character(attr(db[[selected_location]], "last_updated"), usetz = TRUE)), br(),
       "See also: ", lapply(names(location$seealso), function(name) a(name, href = location$seealso[name])), br(),
       sliderInput("days", "Forecast: ",
                      min = Sys.Date(), max = Sys.Date() + 7L,
@@ -100,12 +90,23 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
+
   output$wind_direction <- renderPlot({
-    ggplot_noaa_wind_direction(db$values, days = input$days, windows_size = input$dimension)
+    if (is.null(db[[input$site]])) {
+      location <- locations[[input$site]]
+      url <- noaa_url(lat=location$lat, lon=location$lon)
+      db[[input$site]] <<- read_noaa(url)
+    }
+    ggplot_noaa_wind_direction(db[[input$site]], days = input$days, windows_size = input$dimension)
   })
 
   output$surface_wind <- renderPlot({
-    ggplot_noaa_surface_wind(db$values, days = input$days, windows_size = input$dimension)
+    if (is.null(db[[input$site]])) {
+      location <- locations[[input$site]]
+      url <- noaa_url(lat=location$lat, lon=location$lon)
+      db[[input$site]] <<- read_noaa(url)
+    }
+    ggplot_noaa_surface_wind(db[[input$site]], days = input$days, windows_size = input$dimension)
   })
 }
 
