2417b88f3010221a62e6353d08e42637324b8fbf
Henrik Bengtsson <hb@aroma-project.org>
2019-04-12 18:23:51 -0700

    SITES: Add Blue Rock, Vallejo, CA (a PG site)

--- app.R
+++ app.R
@@ -1,51 +1,9 @@
 library(shiny)
-
 source("utils.R")
+source("locations.R")
 
 timezone("America/Los_Angeles")
 stopifnot(timezone() == "America/Los_Angeles")
-
-locations <- list(
-  "Ed Levin, CA (300 ft & 600 ft)" = list(
-    seealso = c(
-      BHGC = "http://bhgc.org/sites/",
-      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
-    ),
-    lat=  37.4611, lon=-121.8646
-  ),
-
-  "Ed Levin, CA (1750 ft)" = list(
-    seealso = c(
-      BHGC = "http://bhgc.org/sites/",
-      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
-    ),
-    lat=  37.4754, lon=-121.8613
-  ),
-
-  "Mussel Rock/The Dumps, CA (100 ft)" = list(
-    seealso = c(
-      BHGC = "http://bhgc.org/sites/",
-      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
-    ),
-    lat=37.6721641, lon=-122.4939425
-  ),
-
-  "Dunlap, CA (4600 ft)" = list(
-    seealso = c(
-      BHGC = "http://bhgc.org/sites/",
-      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
-    ),
-    lat=36.7641, lon=-119.0976
-  ),
-
-  "Marshall Peak (4200 ft)" = list(
-    seealso = c(
-      BHGC = "http://bhgc.org/sites/",
-      WindSlammer = NA
-    ),
-    lat = 34.210000, lon =-117.302900
-  )
-)
 
 selected_location <- "Ed Levin, CA (1750 ft)"
 if (!exists("db")) db <- list()
---
+++ locations.R
@@ -0,0 +1,49 @@
+locations <- list(
+  "Ed Levin, CA (300 ft & 600 ft)" = list(
+    seealso = c(
+      BHGC = "http://bhgc.org/sites/",
+      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
+    ),
+    lat=  37.4611, lon=-121.8646
+  ),
+
+  "Ed Levin, CA (1750 ft)" = list(
+    seealso = c(
+      BHGC = "http://bhgc.org/sites/",
+      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
+    ),
+    lat=  37.4754, lon=-121.8613
+  ),
+
+  "Mussel Rock/The Dumps, CA (100 ft)" = list(
+    seealso = c(
+      BHGC = "http://bhgc.org/sites/",
+      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
+    ),
+    lat=37.6721641, lon=-122.4939425
+  ),
+
+  "Blue Rock, Vallejo, CA (100 ft)" = list(
+    seealso = c(
+      BHGC = "http://bhgc.org/sites/",
+      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
+    ),
+    lat=38.1379, lon=-122.195
+  ),
+
+  "Dunlap, CA (4600 ft)" = list(
+    seealso = c(
+      BHGC = "http://bhgc.org/sites/",
+      WindSlammer = "http://router.hang-gliding.com/WindSlammer/"
+    ),
+    lat=36.7641, lon=-119.0976
+  ),
+
+  "Marshall Peak (4200 ft)" = list(
+    seealso = c(
+      BHGC = "http://bhgc.org/sites/",
+      WindSlammer = NA
+    ),
+    lat = 34.210000, lon =-117.302900
+  )
+)
